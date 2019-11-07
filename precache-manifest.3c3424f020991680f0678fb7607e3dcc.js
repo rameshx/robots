bac29cefb1a4fa0eebe5e42f724d2dea92c8846e
Ramesh <rameshreddyx@gmail.com>
2019-11-07 18:01:19 +0530

    Updates

---
+++ precache-manifest.3c3424f020991680f0678fb7607e3dcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc56b2a337aba34635d7a95641201fa4",
+    "url": "/Robots-React/index.html"
+  },
+  {
+    "revision": "16ad89ba17370c473bda",
+    "url": "/Robots-React/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "a7ff1694b3b82d6154eb",
+    "url": "/Robots-React/static/css/main.1cdaf4cb.chunk.css"
+  },
+  {
+    "revision": "16ad89ba17370c473bda",
+    "url": "/Robots-React/static/js/2.822a0b48.chunk.js"
+  },
+  {
+    "revision": "a7ff1694b3b82d6154eb",
+    "url": "/Robots-React/static/js/main.c2e32be9.chunk.js"
+  },
+  {
+    "revision": "9a9381943948cc1d0c2b",
+    "url": "/Robots-React/static/js/runtime-main.a91e9d7d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robots-React/static/media/SEGA.12a3db8b.woff"
+  }
+]);
