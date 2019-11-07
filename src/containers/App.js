e2c455bfe47e761c65b855cbb863f19239e3ee03
Ramesh <rameshreddyx@gmail.com>
2019-11-07 18:16:05 +0530

    removed scroll

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
-import Scroll from '../components/Scroll';
 import ErrorBoundary from '../components/ErrorBoundary';
 
 class App extends React.Component {
@@ -31,11 +30,9 @@ class App extends React.Component {
             <div className="tc">
             <h1 className="f1">Robots</h1>
             <SearchBox searchChange={this.onSearchChange}/>
-            <Scroll>
-                <ErrorBoundary>
-                    <CardList robots={filteredRobots}/>
-                </ErrorBoundary>
-            </Scroll>
+            <ErrorBoundary>
+                <CardList robots={filteredRobots}/>
+            </ErrorBoundary>
             </div>
         ) : <h1 className="tc f1">LOADING</h1>;
     }
